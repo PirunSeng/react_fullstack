c2741e5880060c0db58d0cf5acbecf9bd1183c14
Pirun Seng <sengpirun.rain@gmail.com>
2018-09-10 22:18:27 +0700

    Sign in and Forms

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main/index';
+import SignIn from './components/signin/signin';
 
 class Routes extends Component {
   render(){
@@ -17,6 +18,7 @@ class Routes extends Component {
           <Route path="/articles/:id" exact component={NewsArticle} />
           <Route path="/videos/:id" exact component={VideoArticle}/>
           <Route path="/videos" exact component={VideosMain} />
+          <Route path="/sign-in" exact component={SignIn} />
         </Switch>
       </Layout>
     )
